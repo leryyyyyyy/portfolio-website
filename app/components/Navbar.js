849ec956711d88fb84fdeedb0845f1dbedbf4b1c
John Lery Cruz <cruzjohnlery1@gmail.com>
2024-12-12 01:26:47 +0800

    added a bit of smooth effects on open nav in small screen

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -71,8 +71,12 @@ const Navbar = () => {
           </div>
         </div>
 
-        {isOpen && (
-          <div className="md:hidden px-4 pb-4">
+        <div
+          className={`md:hidden overflow-hidden transition-all duration-300 ${
+            isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
+          }`}
+        >
+          <div className="px-4 pb-4">
             <Link
               href="/"
               className="block py-2 hover:text-gray-400"
@@ -113,7 +117,7 @@ const Navbar = () => {
               )}
             </button>
           </div>
-        )}
+        </div>
       </nav>
     </div>
   );
